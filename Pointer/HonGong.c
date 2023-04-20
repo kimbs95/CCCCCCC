9d32358e5de12c9324534d444b96da714cf78211
kimbs95 <lifee456@naver.com>
2023-04-21 00:17:12 +0900

    20230420

--- Pointer/HonGong.c
+++ Pointer/HonGong.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+void swap(int* pa, int* pb);
 
 int main() {
 	// 문자열 두개 
@@ -43,7 +44,7 @@ int main() {
 	scanf("%d", &a);
 	scanf("%d", pa);
 	 */
-
+	/*
 	int a = 10, b = 15, total;
 	double avg;
 	int* pa, * pb;
@@ -67,7 +68,73 @@ int main() {
 	a = 20;				
 	printf("변수 a 값 : %d\n", *c);
 
+	*/
+	printf("##########################\n");
+	printf("##########################\n");
+	printf("##########################\n");
 
+	// sizeof 주소와 포인터의 크기 확인 
+
+	char ch;
+	printf("char 형 변수의 주소크기 : %u\n", sizeof(ch));
+	int in;
+	double db  ;
+	char* pc = &ch;
+	int* pi = &in;
+	double* pd = &db;
+	printf("char 형 변수의 주소크기 : %u\n", sizeof(&ch));	//8
+	printf("int 형 변수의 주소크기 : %u\n", sizeof(&in));	//8
+	printf("double 형 변수의 주소크기 : %u\n", sizeof(&pd));	//8
+	
+	printf("char * 포인터의 크기: %d\n", sizeof(pc));	//8
+	printf("int * 포인터의 크기: %d\n", sizeof(pi));	//8
+	printf("double * 포인터의 크기: %d\n", sizeof(pd));	//8
+
+
+	printf("char * 포인터가 가리키는 변수의 크기 : %d\n", sizeof(*pc));
+	printf("int* 포인터가 가리키는 변수의 크기 : %d\n", sizeof(*pi));
+	printf("double * 포인터가 가리키는 변수의 크기 : %d\n", sizeof(*pd));
+
+
+	
+
+	/*
+	int* p; // 포인터 생성
+	p = (int*)100; //주소에 100 대입 
+	
+
+	// 어디로 값이 들어 갈지 모름
+
+	int* z;
+	z = 100;
+	*/
+
+
+	/* 실행 안됨 변수3 을 선언하지 않고 메모리 아끼며 3을 출력할수있는건가 ? 
+	double kk1 = 3.4;
+	double* bb1 = &kk1;
+	int* ss3;
+	ss3 = (int*)kk1;
+	printf("ex 의 값 : %lf\n", kk1);
+	printf("ex2 의 값 : %u\n", *ss3);
+	*/
+
+
+	// 포인터를 통해 두 변수의 값 교환 
+	printf("포인터를 통해 두 변수의 값 교환 \n");
+
+	int a = 10, b = 20;
+	swap(&a, &b);
+	printf("a:%d , b:%d	\n",a,b );
 
 	return 0;
+}
+
+void swap(int* pa, int* pb) {
+	int temp;
+	temp = *pa;
+
+	*pa = *pb;
+	*pb = temp;
+
 }
